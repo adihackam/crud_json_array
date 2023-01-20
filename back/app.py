5d5d599158c189e7fe7659a228754cde7e4eb7d8
Adi Hackam <aaaadi01@gmail.com>
2023-01-20 16:10:17 +0200

    initial commit

---
+++ back/app.py
@@ -0,0 +1,76 @@
+import json
+import os 
+from flask import Flask, request ,redirect, render_template, url_for
+from flask_cors import CORS
+
+app = Flask(__name__, template_folder='../front')
+CORS(app)
+
+myFile= "./back/students.json"
+students={}
+
+def loadFromFile():
+    isExist = os.path.exists(myFile)
+    if isExist:
+        with open(myFile, 'r') as openFile:
+            students = json.load(openFile)
+        return students
+    return []
+
+def save2File(students):
+    jsonString = json.dumps(students, indent=4)
+    with open(myFile, "w") as outfile:
+	    outfile.write(jsonString)
+
+
+@app.route('/students/', methods = ['GET', 'POST','DELETE'])
+@app.route('/students/<id>', methods = ['GET', 'POST','DELETE','PUT'])
+def crude_students(id=-1):
+    if request.method == 'POST':
+        request_data = request.get_json()
+        city = request_data['city']
+        name= request_data["name"]
+
+        students = loadFromFile() 
+        maxId = 0
+        for stu in students:
+            if stu['id'] > maxId : 
+                maxId = stu['id']
+        newStudent = {"id":maxId + 1, "name": name, "city": city}
+        students.append(newStudent)
+        save2File(students)
+        return {"msg":"new student was added"} 
+
+    if request.method == 'GET':
+        res=[]
+        students = loadFromFile() 
+        for student in students:
+            res.append({"city":student['city'],"id":student['id'],"name":student['name']})
+        return  res
+        # return  (json.dumps(res))
+    if request.method == 'DELETE': 
+        students = loadFromFile() 
+        for stu in students:
+            if stu['id'] == int(id): 
+                students.remove(stu)
+        save2File(students)
+        return {"msg":"row deleted"}
+
+    if request.method == 'PUT':
+        request_data = request.get_json()
+        students = loadFromFile() 
+        for stu in students:
+            if stu['id'] == int(id): 
+                stu['city'] = request_data['city']
+                stu['name']= request_data['name']
+        save2File(students)
+        return {"msg":"student updated"}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+ 
+if __name__ == '__main__':
+    # with app.app_context():
+    #     db.create_all()
+    app.run(debug = False)
